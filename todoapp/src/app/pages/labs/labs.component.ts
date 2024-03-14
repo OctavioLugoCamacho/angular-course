e8a5e868128463a6d8bf1207746e9bd91c354e62
Octavio Lugo Camacho <octavio@makingdevs.com>
2024-03-14 17:53:17 -0600

    [ADD] Create components

---
+++ todoapp/src/app/pages/labs/labs.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
+})
+export class LabsComponent {
+  welcome = 'Curso de Platzi';
+  tasks = [
+    'Instalé Angular con npm',
+    'Aprendí a levantar el Server',
+    'Estoy viendo cómo mostrar variables en el front'
+  ]
+}
